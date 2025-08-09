45d977f1bd1c14d124eb225468e535b3d02b7d73
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-08-08 22:56:13 -0300

    Create test script to make repetitive curl requests to the payment endpoint.

---
+++ tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Loop que será executado 100 vezes
+# Envia uma requisição POST para o endpoint /payments
+# Define o cabeçalho Content-Type como application/json
+# Define o corpo da requisição como um JSON
+# Executa o comando em segundo plano, permitindo que o loop continue
+# Pausa a execução do loop por 0,1 segundos antes de enviar a próxima requisição 
+for i in {1..100}; do curl --location 'http://localhost:9999/payments' \
+ --header 'Content-Type: application/json'  \
+ --data '{
+            "correlationId": "4a7901b8-7d26-4d9d-aa19-4dc1c7cf60b3",
+            "amount": 19.90
+    }' & sleep 0.1; 
+done
