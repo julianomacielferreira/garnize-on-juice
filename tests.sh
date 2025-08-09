a7b7069825dd049c82dd6bfa7060e40d9b16253f
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-08-09 15:13:47 -0300

    Remove duplicate code.

--- tests.sh
+++ tests.sh
@@ -10,9 +10,11 @@
 # O script aguarda todas as requisições em segundo plano terminarem antes de sair.
 NUM_REQUISICOES=100
 
-PAYMENTS_ENDPOINT="http://localhost:9999/payments"
+BASE_URL="http://localhost:9999"
 
-PAYMENTS_SUMMARY_ENDPOINT="http://localhost:9999/payments-summary"
+PAYMENTS_ENDPOINT="${BASE_URL}/payments"
+
+PAYMENTS_SUMMARY_ENDPOINT="${BASE_URL}/payments-summary"
 
 PAYLOAD='{
   "correlationId": "4a7901b8-7d26-4d9d-aa19-4dc1c7cf60b3",
@@ -23,13 +25,13 @@ FROM_DATE="2025-08-09T00%3A15%3A08.174Z"
 TO_DATE="2025-08-09T00%3A15%3A08.174Z"
 
 for i in $(seq 1 $NUM_REQUISICOES); do
-  curl --location "$PAYMENTS_ENDPOINT" \
+  curl --location --request POST "$PAYMENTS_ENDPOINT" \
     --header 'Content-Type: application/json' \
     --data "$PAYLOAD" &
   
   sleep 0.05
   
-  curl --location "$PAYMENTS_SUMMARY_ENDPOINT?from=$FROM_DATE&to=$TO_DATE" > /dev/null &
+  curl --location --request GET "$PAYMENTS_SUMMARY_ENDPOINT?from=$FROM_DATE&to=$TO_DATE" > /dev/null &
   
   # Aguarde um pouco antes da próxima requisição
   sleep 0.05
