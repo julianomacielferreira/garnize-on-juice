8a6255af6135afae28dad4154e93e4de56b17d69
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-08-09 15:19:01 -0300

    Rename test scripts.

--- test-purge-databse.sh
+++ test-purge-databse.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# Define a URL base para as requisições e o endpoint para limpar o banco
 BASE_URL="http://localhost:9999"
-
 PURGE_PAYMENTS_ENDPOINT="${BASE_URL}/purge-payments"
 
+# Envia uma requisição POST para o endpoint de limpeza o banco
 curl --location --request POST "$PURGE_PAYMENTS_ENDPOINT" --header 'Content-Type: application/json' 
